552e50a0a481fdc140a8439ad1a6999ba1c6dd5c
Amit <Amitkumar320321@gmail.com>
2023-06-04 14:17:53 +0500

    fix all pages title name

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -13,7 +13,7 @@
   <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
 
-    <title>Add Category</title>
+    <title>Category</title>
   @include('admin.head')
   </head>
 
